80854174ca31f95f8723341ba26234ef7ae39571
labCap <banlazthosow@gmail.com>
2022-12-14 13:21:13 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const passEl = "1234567890qwertyuiopasdfghjklzxcvbnm~!@#^*_-+=";
+
+  const generatePass = (arr, length) => {
+    let arrPassEl = arr.split("");
+    let pass = [];
+
+    for (let i = 0; i < length; i++) {
+      const element = arrPassEl[i];
+      pass.push(arrPassEl[Math.floor(Math.random() * arrPassEl.length)]);
+    }
+    return pass.join("");
+  };
+  console.log(generatePass(passEl, 10));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="text"></div>
+      <form className="form">
+        <input type="text" />
+        <button>Generate</button>
+      </form>
     </div>
   );
 }
