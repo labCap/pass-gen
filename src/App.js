420055d73950635d9a6ca28230079bf876a4e481
labCap <banlazthosow@gmail.com>
2022-12-14 17:18:34 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   const passEl = "1234567890qwertyuiopasdfghjklzxcvbnm~!@#^*_-+=";
+
+  const [pass, setPass] = useState("");
+  const [userLength, setUserLength] = useState("8");
 
   const generatePass = (arr, length) => {
     let arrPassEl = arr.split("");
     let pass = [];
 
     for (let i = 0; i < length; i++) {
-      const element = arrPassEl[i];
       pass.push(arrPassEl[Math.floor(Math.random() * arrPassEl.length)]);
     }
+    x;
     return pass.join("");
   };
-  console.log(generatePass(passEl, 10));
+
+  const handleSubmit = (e, length) => {
+    e.preventDefault();
+    if (length >= 6) {
+      setPass(generatePass(passEl, length));
+    }
+  };
 
   return (
-    <div className="container">
-      <div className="text"></div>
-      <form className="form">
-        <input type="text" />
+    <main>
+      <div className="text">
+        {pass === "" ? "Your password will appear here" : pass}
+      </div>
+      <form className="form" onSubmit={(e) => handleSubmit(e, userLength)}>
+        <label htmlFor="password-length" className="label-range">
+          {userLength}
+        </label>
+        <input
+          type="range"
+          className="input-range"
+          id="password-length"
+          value={userLength}
+          step={1}
+          min={6}
+          max={100}
+          onChange={(e) => setUserLength(e.target.value)}
+        />
+
         <button>Generate</button>
       </form>
-    </div>
+    </main>
   );
 }
 
